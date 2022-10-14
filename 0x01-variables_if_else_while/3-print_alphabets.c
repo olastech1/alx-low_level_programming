7205378d6a776d39d752dbf0cac2964b01b4483b
olaniyi iyanu <info.olastech@gmail.com>
2022-10-14 13:02:19 -0700

    print al c

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -1,28 +1,30 @@
 #include <stdio.h>
 #include <ctype.h>
 /**
- * *main - Program to print alphabet letters in all cases followed by new line
- * *
- * *Return: return 0
- * */
+ *main - Program to print alphabet letters in all cases followed by new line
+ *
+ *Return: return 0
+ */
 int main(void)
 {
-		int letter;
-
-			for (letter = 'a'; letter <= 'z'; letter++)
-					{
-								letter = tolower(letter);
-										putchar(letter);
-												if (letter == 'z')
-															{
-																			letter = 'A';
-																						for (; letter <= 'Z'; letter++)
-																										{
-																															putchar(letter);
-																																		}
-																									break;
-																											}
-													}
-				putchar('\n');
-					return (0);
+	int letter;
+	
+	for (letter = 'a'; letter <= 'z'; letter++)
+	{
+		letter = tolower(letter);
+		putchar(letter);
+		if (letter == 'z')
+		{
+			letter = 'A';
+			for (; letter <= 'Z'; letter++)
+																								{
+																									putchar(letter);
+																								}
+																								break;
+		
+		}
+	
+	}
+	putchar('\n');
+	return (0);
 }
