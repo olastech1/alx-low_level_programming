63e7186fc89dafc86c0a4260df94ffb0eed52f17
olaniyi iyanu <info.olastech@gmail.com>
2022-10-14 08:35:34 -0700

    puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,13 +1,13 @@
 #include <stdio.h>
-
+#include <unistd.h>
 /**
- * main - Prints quote
- * 
- * Return: zero on success
- *
- */
+ * * main - main function in the task
+ * *
+ * *
+ * *Return: return 1
+ * */
 int main(void)
 {
-		puts("\"Programming is like building a multilingual puzzle");
-			return (0);
+		write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+			return (1);
 }
