20aa38383a612178d31c51e6f23a575754983273
olaniyi iyanu <info.olastech@gmail.com>
2022-10-14 08:17:10 -0700

    alx c project

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,18 +1,11 @@
-#include <stdio.h>
-#include <string.h>
-
+#include<stdio.h>
 /**
- * *main - Main function of my project
- * *
- * *
- * *Return: return 0 everytime
- * */
+ *  * main - Entry point
+ *   *
+ *    * Return: Always 0 (Success)
+ *     */
 int main(void)
 {
-
-		char displayed[52];
-
-			strcpy(displayed, "\"Programming is like building a multilingual puzzle");
-				puts(displayed);
-					return (0);
+		puts("\"Programming is like building a multilingual puzzle");
+			return (0);
 }
