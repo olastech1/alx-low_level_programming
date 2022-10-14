5219a600380d76b0d4a066b7620ed24e2b3c2bfe
olaniyi iyanu <info.olastech@gmail.com>
2022-10-14 08:38:04 -0700

    put

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,13 +1,18 @@
 #include <stdio.h>
-#include <unistd.h>
+#include <string.h>
+
 /**
- * * main - main function in the task
+ * *main - Main function of my project
  * *
  * *
- * *Return: return 1
+ * *Return: return 0 everytime
  * */
 int main(void)
 {
-		write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
-			return (1);
+
+		char displayed[52];
+
+			strcpy(displayed, "\"Programming is like building a multilingual puzzle");
+				puts(displayed);
+					return (0);
 }
