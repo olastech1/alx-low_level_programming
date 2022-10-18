98914a74fa635313bb0572f476ae31105aa9e709
olaniyi iyanu <info.olastech@gmail.com>
2022-10-18 05:46:05 -0700

    function that prints all natural numbers from n to 98, followed by a new line.

--- 0x02-functions_nested_loops/10-add.c
+++ 0x02-functions_nested_loops/10-add.c
@@ -1,9 +1,11 @@
 #include "main.h"
 /**
  *add - sums up two numbers
+ *@n:first integer
+ *@m:second integer
  *Return: returns 0
  */
 int add(int n, int m)
 {
-	return (n + m);
+return (n + m);
 }
